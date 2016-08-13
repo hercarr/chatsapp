7f9327a10fe0dc3460b76ffcadf47b85823f2c89
victor.hernandez <victor.hernandez@qacg.com>
2016-08-12 22:56:43 -0600

    The color scheme was added and the styles was modified to config material design

--- app/src/main/java/mx/hercarr/chatsapp/activities/MainActivity.java
+++ app/src/main/java/mx/hercarr/chatsapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package mx.hercarr.chatsapp;
+package mx.hercarr.chatsapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import mx.hercarr.chatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
